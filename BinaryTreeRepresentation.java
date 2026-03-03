84373f44dfc34fba6cbc4c7ee918a1be5ae63913
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-03-03 17:24:16 +0530

    Representation

--- BinaryTreeRepresentation.java
+++ BinaryTreeRepresentation.java
@@ -11,7 +11,10 @@ class Node{
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
-        root.right.left = new Node(4);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
     }
 
 }
